46c19a5b17687ace71bf8d3e95eb245144365ca3
spencerlambert <slambert@corp.digis.net>
2014-05-25 14:45:50 -0600

    Moving a define out of config.php and into defines.php

--- php/setup/config_sample.php
+++ php/setup/config_sample.php
@@ -4,11 +4,10 @@ define("MASTER_DB_HOST","localhost");
 define("MASTER_DB_USER","root");
 define("MASTER_DB_PASS","mysqlpass");
 define("MASTER_DB_NAME","master");
-// SSL is not supported in PDO at this time
+// SSL is not implemented in the ResourceManager class at this time
 define("MASTER_DB_SSL",false);
 
 define("MASTER_DB_TABLE_PREFIX", '');
-define("MASTER_DB_NAME_WITH_PREFIX", MASTER_DB_NAME.".".MASTER_DB_TABLE_PREFIX); //Used in MySQL statements;
 
 define("TEMPLATE_NAME", 'default');
 define("DEBUG",false);
