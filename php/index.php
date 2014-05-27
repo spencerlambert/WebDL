d6b644047ddecb36275ac2aaa1e3e6500c5f442e
spencerlambert <slambert@corp.digis.net>
2014-05-26 23:09:39 -0600

    updating class name

--- php/index.php
+++ php/index.php
@@ -2,6 +2,6 @@
     define('ABSPATH', dirname(__FILE__).'/');
     require_once(ABSPATH.'setup/init.php');
     
-    $controller = ControllerBasic::get_requested_controller();
+    $controller = ControllerBase::get_requested_controller();
     $controller->load_page();
 ?>
