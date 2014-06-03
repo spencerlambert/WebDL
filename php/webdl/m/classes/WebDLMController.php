ce96f2de61dd57ec3b7be95f4c335ec84ca72b1f
spencerlambert <slambert@corp.digis.net>
2014-06-03 15:33:15 -0600

    Making the DLM result it's own class

--- php/webdl/m/classes/WebDLMController.php
+++ php/webdl/m/classes/WebDLMController.php
@@ -33,9 +33,9 @@ class WebDLMController {
     
     public function dlm_request($request) {
         
-        // An array to keep all the data from the DLM requests.
-        $data = array();
-        $row_links = array();
+        // The result of the request.
+        $result = new WebDLMResult();
+        
 
         // All DLMs needed for this request
         $required_dlms = $this->tree->get_required_dlms($request);
@@ -95,9 +95,9 @@ class WebDLMController {
             //Add any matches that need to link to other DLMs from our first results.
             foreach ($connector_run as $dlm_id) {
                 if (isset($data[$dlm_id])) continue; // Don't rerun any dlm that has already been run
-                $data[$dlm_id] = $this->run_one_dlm($dlm_id, $request);
+                $result = $this->run_one_dlm($dlm_id, $request, $result);
                 // Now look for keys that match connectors and add the matches to the request.
-                foreach ($data[$dlm_id]['DATA'] as $row) {
+                foreach ($result->get_dlm_data($dlm_id) as $row) {
                     foreach ($needed_connectors as $connectors) {
                         foreach ($connectors as $connector) {
                             // Add the Primary Key links
@@ -105,9 +105,7 @@ class WebDLMController {
                                 foreach($connector->get_foreign_key($row[$connector->c_id]) as $val_f) {
                                     // Add each key as a match value so we get the needed rows when the other DLM is run.
                                     $request->push_match($connector->c_id_f, $val_f, 'OR');
-                                    if (!isset($row_links[$connector->c_id_f."-".$val_f]))
-                                        $row_links[$connector->c_id_f."-".$val_f] = array();
-                                    $row_links[$connector->c_id_f."-".$val_f][] = $row;
+                                    $result->push_column_link($connector->c_id_f, $val_f, $row);
                                 }
                             }
                             // Add the Foreign Key Links
@@ -115,9 +113,7 @@ class WebDLMController {
                                 foreach($connector->get_primary_key($row[$connector->c_id_f]) as $val_p) {
                                     // Add each key as a match value so we get the needed rows when the other DLM is run.
                                     $request->push_match($connector->c_id, $val_p, 'OR');
-                                    if (!isset($row_links[$connector->c_id."-".$val_p]))
-                                        $row_links[$connector->c_id."-".$val_p] = array();
-                                    $row_links[$connector->c_id."-".$val_p][] = $row;
+                                    $result->push_column_link($connector->c_id, $val_p, $row);
                                 }
                             }
                         }
@@ -131,101 +127,23 @@ class WebDLMController {
         // Run any remaining DLMs
         foreach ($required_dlms as $dlm_id) {
             if (isset($data[$dlm_id])) continue; // Don't rerun any dlm that has already been run
-            $data[$dlm_id] = $this->run_one_dlm($dlm_id, $request);
+            $result = $this->run_one_dlm($dlm_id, $request, $result);
         }
         
-        // Join the data if needed.
-        // TODO: This is really not working...  Need to get it right.
-        // TODO: This will only join two DLMs, need to add logic for joining more together.
-        $join = array();
-        if (count($row_links) != 0) { 
-            foreach ($required_dlms as $dlm_id) {
-                foreach ($data[$dlm_id]['DATA'] as $row) {
-                    foreach ($row as $c_id=>$c_val) {
-                        $name = $c_id."-".$c_val;
-                        if (isset($row_links[$name])) {
-                            if (!isset($join[$dlm_id]))
-                                $join[$dlm_id] = array();
-                            foreach ($row_links[$name] as $id=>$link) {
-                                $join[$dlm_id][] = array_merge($row, $link);                            
-                            }
-                        }
-                    }
-                }
-            }
-        }// end if count()
-        
-        // Merge the rows that match the connector values.
-        $merge = array();
-        foreach ($join as $dlm_id=>$m_ary) {
-            foreach ($join as $dlm_id_f=>$m_ary_f) {
-                if ($dlm_id == $dlm_id_f) continue;
-                foreach ($m_ary as $row) {
-                    foreach($m_ary_f as $row_f) {
-                        //if all the rows in one array match the other, then merge the extra data.
-                        $all_match = true;
-                        foreach ($row as $col=>$val) {
-                            if (isset($row_f[$col]))
-                                if ($row_f[$col] !== $val)
-                                    $all_match = false;
-                        }
-                        if ($all_match)
-                            $merge[] = array_merge($row, $row_f);
-                    }
-                }
-            }
-        }
-
-        // Make sure the rows with the most columns are reviewed first,
-        // this keeps the rows with partially data out.
-        usort($merge, "WebDLMController::sort_by_array_size");
-
-        // Filter only the rows that have unique values.
-        $filter = array();
-        if (count($merge) > 0) {
-            // pre-load the filter array with the top returned value, this gives us something to compare with.
-            $k_sort = $merge[0];
-            ksort($k_sort);
-            $filter[] = $k_sort;
-            foreach ($merge as $row_m) {
-                $is_uniqe_row = true;
-                foreach($filter as $row_f) {
-                    $test = array_diff_assoc($row_m, $row_f);
-                    // If there are no real differences, then we don't have a new unique row.
-                    if (count($test) == 0) {
-                        $is_uniqe_row = false;
-                        break;
-                    }
-                }
-                // We have a new row to add to our list
-                if ($is_uniqe_row) {
-                    $k_sort = $row_m;
-                    ksort($k_sort);
-                    $filter[] = $k_sort;
-                }
-            }
-        }
-        
-        // TODO: This has only been tested on a three DLM join, it may not fully join more DLMs into a single row.
-        
-        $data['JOIN']['DATA'] = $filter;
         
         // Return the data
-        return $data;
+        return $result;
     }
     
-    static public function sort_by_array_size($a, $b) {
-        return count($a) < count($b);
-    }
-    private function run_one_dlm($dlm_id, $request) {
+    private function run_one_dlm($dlm_id, $request, $result) {
         $type = strtolower($request->get_type());
-        $data = array();
-        $data['IS_ONLINE'] = $this->dlms[$dlm_id]->is_ready();
-        $data['DATA'] = "";
-        if ($data['IS_ONLINE'] === true) {
-            $data['DATA'] = $this->dlms[$dlm_id]->$type($request);
+        $is_online = $this->dlms[$dlm_id]->is_ready();
+        if ($is_online) {
+            $result->set_dlm_result($dlm_id, $this->dlms[$dlm_id]->$type($request));
+        } else {
+            $result->set_dlm_result($dlm_id, NULL, false);            
         }
-        return $data;
+        return $result;
     }
     
 }
---
+++ php/webdl/m/classes/WebDLMResult.php
@@ -0,0 +1,118 @@
+<?php
+class WebDLMResult {
+
+    public $dlm_result = array();
+    protected $row_links = array();
+    
+    public function __construct() {
+        
+    }
+    
+    public function set_dlm_result($dlm_id, $data, $is_online=true) {
+        // Save the data returned by the DLM for later reterval or joining.
+        if (!isset($this->dlm_result[$dlm_id])) {
+            $class = new stdClass();
+            $class->is_online = $is_online;
+            $class->data = $data;
+            $this->dlm_result[$dlm_id] = $class;
+        }
+    }
+    
+    public function push_column_link($c_id, $val, $data) {
+        if (!isset($this->row_links[$c_id."-".$val]))
+            $this->row_links[$c_id."-".$val] = array();
+        $row_links[$c_id."-".$val][] = $data;        
+    }
+    
+    public function get_dlm_data($dlm_id) {
+        if (!isset($this->dlm_result[$dlm_id])) return false;
+        if (!$this->dlm_result[$dlm_id]->is_online) return false;
+        return $this->dlm_result[$dlm_id]->data;
+    }
+    
+    public function get_joined_data() {
+    
+        // Join the data if needed.
+        // TODO: This is really not working...  Need to get it right.
+        // TODO: This will only join two DLMs, need to add logic for joining more together.
+        $join = array();
+        if (count($this->row_links) != 0) { 
+            foreach ($this->dlm_result as $dlm_id=>$result) {
+                if (!$result->is_online) continue;
+                foreach ($result->data as $row) {
+                    foreach ($row as $c_id=>$c_val) {
+                        $name = $c_id."-".$c_val;
+                        if (isset($this->row_links[$name])) {
+                            if (!isset($join[$dlm_id]))
+                                $join[$dlm_id] = array();
+                            foreach ($row_links[$name] as $id=>$link) {
+                                $join[$dlm_id][] = array_merge($row, $link);                            
+                            }
+                        }
+                    }
+                }
+            }
+        }// end if count()
+        
+        // Merge the rows that match the connector values.
+        $merge = array();
+        foreach ($join as $dlm_id=>$m_ary) {
+            foreach ($join as $dlm_id_f=>$m_ary_f) {
+                if ($dlm_id == $dlm_id_f) continue;
+                foreach ($m_ary as $row) {
+                    foreach($m_ary_f as $row_f) {
+                        //if all the rows in one array match the other, then merge the extra data.
+                        $all_match = true;
+                        foreach ($row as $col=>$val) {
+                            if (isset($row_f[$col]))
+                                if ($row_f[$col] !== $val)
+                                    $all_match = false;
+                        }
+                        if ($all_match)
+                            $merge[] = array_merge($row, $row_f);
+                    }
+                }
+            }
+        }
+
+        // Make sure the rows with the most columns are reviewed first,
+        // this keeps the rows with partially data out.
+        usort($merge, "WebDLMResult::sort_by_array_size");
+
+        // Filter only the rows that have unique values.
+        $filter = array();
+        if (count($merge) > 0) {
+            // pre-load the filter array with the top returned value, this gives us something to compare with.
+            $k_sort = $merge[0];
+            ksort($k_sort);
+            $filter[] = $k_sort;
+            foreach ($merge as $row_m) {
+                $is_uniqe_row = true;
+                foreach($filter as $row_f) {
+                    $test = array_diff_assoc($row_m, $row_f);
+                    // If there are no real differences, then we don't have a new unique row.
+                    if (count($test) == 0) {
+                        $is_uniqe_row = false;
+                        break;
+                    }
+                }
+                // We have a new row to add to our list
+                if ($is_uniqe_row) {
+                    $k_sort = $row_m;
+                    ksort($k_sort);
+                    $filter[] = $k_sort;
+                }
+            }
+        }
+        
+        // TODO: This has only been tested on a three DLM join, it may not fully join more DLMs into a single row.
+        
+        return $filter;
+    }
+
+    static public function sort_by_array_size($a, $b) {
+        return count($a) < count($b);
+    }
+    
+}
+?>
