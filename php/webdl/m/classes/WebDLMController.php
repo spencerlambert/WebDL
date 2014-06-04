25c1dd39ef8592c65cd7269328d6e18a70479114
spencerlambert <slambert@corp.digis.net>
2014-06-04 13:07:18 -0600

    Fixing self:: calls

--- php/webdl/m/classes/WebDLMController.php
+++ php/webdl/m/classes/WebDLMController.php
@@ -23,18 +23,18 @@ class WebDLMController {
         $sth->execute();
 
         foreach ($sth->fetchAll(PDO::FETCH_ASSOC) as $row) {
-            self::dlms[$row['DLMID']] = new $row['ClassName']($row['DLMID']);
+            self::$dlms[$row['DLMID']] = new $row['ClassName']($row['DLMID']);
         }
                 
         // Get a tree that contains all DLMs
-        self::tree = new WebDLMTree();
+        self::$tree = new WebDLMTree();
 
         // Get an array with all the connectors
         // Not sure how much I like how the array is put together.  Need to think
         // up a better way of doing this.
-        self::connectors = WebDLMConnector::get_connectors();
+        self::$connectors = WebDLMConnector::get_connectors();
         
-        self::initialize = true;
+        self::$initialize = true;
 
     }
     
@@ -46,19 +46,19 @@ class WebDLMController {
         
 
         // All DLMs needed for this request
-        $required_dlms = self::tree->get_required_dlms($request);
+        $required_dlms = self::$tree->get_required_dlms($request);
 
         // There are special considerations that have not yet been figured out
         // for other types of requests.  See comments below.
         if ($request->get_type() == "GET") {
             
             //Are any connectors involved?
-            $required_tables = self::tree->get_required_tables($request);
+            $required_tables = self::$tree->get_required_tables($request);
             $needed_connectors = array();
             foreach ($required_tables as $t_id_p) {
                 foreach ($required_tables as $t_id_f) {
-                    if (isset(self::connectors['BY_TABLE_TO_TABLE'][$t_id_p][$t_id_f]))
-                        $needed_connectors[] = self::connectors['BY_TABLE_TO_TABLE'][$t_id_p][$t_id_f];
+                    if (isset(self::$connectors['BY_TABLE_TO_TABLE'][$t_id_p][$t_id_f]))
+                        $needed_connectors[] = self::$connectors['BY_TABLE_TO_TABLE'][$t_id_p][$t_id_f];
                 }
             }
             
@@ -67,8 +67,8 @@ class WebDLMController {
             $connector_dlms = array();
             foreach ($needed_connectors as $connectors) {
                 foreach ($connectors as $connector) {
-                    $connector_dlms[self::tree->columns[$connector->c_id]->dlm_id] = self::tree->columns[$connector->c_id]->dlm_id;
-                    $connector_dlms[self::tree->columns[$connector->c_id_f]->dlm_id] = self::tree->columns[$connector->c_id_f]->dlm_id;
+                    $connector_dlms[self::$tree->columns[$connector->c_id]->dlm_id] = self::$tree->columns[$connector->c_id]->dlm_id;
+                    $connector_dlms[self::$tree->columns[$connector->c_id_f]->dlm_id] = self::$tree->columns[$connector->c_id_f]->dlm_id;
                     if (!$request->is_column_included($connector->c_id))
                         $request->push_column($connector->c_id);
                     if (!$request->is_column_included($connector->c_id_f))
@@ -79,7 +79,7 @@ class WebDLMController {
             
             //Run any dlms in which we are matching on fisrt.
             $connector_run = array();
-            foreach (self::tree->get_match_dlms($request) as $dlm_id) {
+            foreach (self::$tree->get_match_dlms($request) as $dlm_id) {
                 $connector_run[] = $dlm_id;
             }
             //Put the DLMs in run order so we can filter out other data as we go.
@@ -87,10 +87,10 @@ class WebDLMController {
             while (!$have_all) {
                 foreach ($needed_connectors as $connectors) {
                     foreach ($connectors as $connector) {
-                        if (in_array(self::tree->columns[$connector->c_id]->dlm_id, $connector_run)  && !in_array(self::tree->columns[$connector->c_id_f]->dlm_id, $connector_run))
-                            $connector_run[] = self::tree->columns[$connector->c_id_f]->dlm_id;
-                        if (in_array(self::tree->columns[$connector->c_id_f]->dlm_id, $connector_run)  && !in_array(self::tree->columns[$connector->c_id]->dlm_id, $connector_run))
-                            $connector_run[] = self::tree->columns[$connector->c_id]->dlm_id;
+                        if (in_array(self::$tree->columns[$connector->c_id]->dlm_id, $connector_run)  && !in_array(self::$tree->columns[$connector->c_id_f]->dlm_id, $connector_run))
+                            $connector_run[] = self::$tree->columns[$connector->c_id_f]->dlm_id;
+                        if (in_array(self::$tree->columns[$connector->c_id_f]->dlm_id, $connector_run)  && !in_array(self::$tree->columns[$connector->c_id]->dlm_id, $connector_run))
+                            $connector_run[] = self::$tree->columns[$connector->c_id]->dlm_id;
                     }
                 }
                 $have_all = true;
@@ -145,9 +145,9 @@ class WebDLMController {
     
     private static function run_one_dlm($dlm_id, $request, $result) {
         $type = strtolower($request->get_type());
-        $is_online = self::dlms[$dlm_id]->is_ready();
+        $is_online = self::$dlms[$dlm_id]->is_ready();
         if ($is_online) {
-            $result->set_dlm_result($dlm_id, self::dlms[$dlm_id]->$type($request));
+            $result->set_dlm_result($dlm_id, self::$dlms[$dlm_id]->$type($request));
         } else {
             $result->set_dlm_result($dlm_id, NULL, false);            
         }
