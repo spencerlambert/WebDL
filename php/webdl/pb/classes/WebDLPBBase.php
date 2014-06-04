37d28bfecc86b9619af2d2f69b4690029cb17205
spencerlambert <slambert@corp.digis.net>
2014-06-04 14:44:07 -0600

    Working on self contained Page Block that shows a table and can update content via anqular ajax.

--- php/webdl/pb/classes/WebDLPBBase.php
+++ php/webdl/pb/classes/WebDLPBBase.php
@@ -5,33 +5,42 @@
  */
 class WebDLPBBase {
     protected $html;
-    protected $dom_node;
-    protected $controller;
-    
-    public function __construct($dom_node, $controller) {
-        $this->dom_node = $dom_node;
-        $this->controller = $controller;
-        if (get_class($this) == "WebPBBase") {
-            $msg = 'Looks like a tag named "'.$dom_node->nodeName.'" has been used in the page content XML, but a corresponding PHP class does not exist. A class that extents WebPBBase with the name "'.$dom_node->nodeName.'" needs to be created.  Look in the '.ABSPATH.'webpb/ folder.';
-            WebDLUserMessage::output($msg, 'WebDLPBBase.php');
-        }
+    protected $unique_id;
+
+    public function __construct($unique_id) {
+        $this->unique_id = $unique_id;
     }
 
     public function get_html() {
         return $this->html;
     }
 
-    /***
-     * Doesn't work. The examples online show the overloaded data in an array.  I dont' want that.
-     * 
-    public function __get($name) {
-        if (isset($this->$name)) {
-            return $this->$name;
-        } else {
-            UserMessage::output('Error: Trying to get a varible named "'.$name.'" from '.get_class($this).' that is non-existent.');
-        }
+    private function get_angularjs() {
+        $js = '
+            <script>
+                function '.$this->unique_id.'Ctrl($scope) {
+                    $scope.data = ;
+                    $scope.columns = ;
+                    $scope.matches = ;
+                    
+                    $scope.reset_matches = function () {
+                        $scope.matches = [];
+                    }
+                    
+                    $scope.push_match = function (match_json) {
+                        $scope.matches.push(match_json);
+                    }
+                    
+                    $scope.update = function () {
+                        
+                    }
+                    
+                }
+            </script>
+        ';
+        return $js;
     }
-     */
+
     
 }
 
