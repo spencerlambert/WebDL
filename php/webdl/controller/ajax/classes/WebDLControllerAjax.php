3e46c6efb9c6117e02c8243e3f51e4a31937a024
Spencer Lambert <github@webdl.co>
2014-06-08 15:02:04 -0600

    Renaming index.php to webdl.php, to enable installing the code within exsisting PHP projects.

---
+++ php/webdl/controller/ajax/classes/WebDLControllerAjax.php
@@ -0,0 +1,12 @@
+<?php
+class WebDLControllerAjax extends WebDLControllerBase {
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function load_page() {
+        
+    }
+    
+}
+?>
