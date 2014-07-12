fb6ffe23af087d0b5f76019b09c2103e601b32cf
Spencer Lambert <spence@digis.net>
2014-07-11 22:48:37 -0600

    Update webdl.php
    Setting library in the include path.

--- php/webdl.php
+++ php/webdl.php
@@ -1,5 +1,6 @@
 <?php
     define('WEBDL_ABSPATH', dirname(__FILE__).'/');
+    set_include_path(get_include_path() . PATH_SEPARATOR . WEBDL_ABSPATH . 'library');
     require_once(WEBDL_ABSPATH.'webdl/setup/init.php');
     
     $controller = WebDLControllerBase::get_requested_controller();
