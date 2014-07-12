269903eefb59eb9b9c96cd5ecd9ef04deaf46707
sirhanshafahath <sirhan@iozeta.com>
2014-07-12 22:46:55 +0530

    Only load the Zend, if its already not included via php.in

--- php/webdl.php
+++ php/webdl.php
@@ -1,6 +1,8 @@
 <?php
     define('WEBDL_ABSPATH', dirname(__FILE__).'/');
-    set_include_path(get_include_path() . PATH_SEPARATOR . WEBDL_ABSPATH . 'webdl/library');
+    if (!class_exists('Zend_Loader', true)) {
+        set_include_path(get_include_path() . PATH_SEPARATOR . WEBDL_ABSPATH . 'webdl/library');
+    }
     require_once(WEBDL_ABSPATH.'webdl/setup/init.php');
     
     $controller = WebDLControllerBase::get_requested_controller();
