cf9b50dceb928847a92a1a670f17bdc46ac53464
Spencer Lambert <spence@digis.net>
2014-07-30 14:36:20 -0600

    Adding db

--- php/webdl_update_cache.php
+++ php/webdl_update_cache.php
@@ -2,6 +2,7 @@
     define('WEBDL_ABSPATH', dirname(__FILE__).'/');
     require_once(WEBDL_ABSPATH.'webdl/setup/init.php');
     
+    $db = WebDLResourceManager::get("DB_MASTER_PDO");
 
     $sql = "SELECT RecordModelID FROM ".WEBDL_MASTER_DB_NAME_WITH_PREFIX."DLMRecordModel";
     $sth = $db->prepare($sql);
