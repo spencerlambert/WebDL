1c7a89d2be60ca76f5d1cba49bbba5692897da81
Spencer Lambert <spence@digis.net>
2014-12-01 19:10:50 -0700

    Fixing var name

--- php/webdl/m/WebDLMLDAP/classes/WebDLMLDAP.php
+++ php/webdl/m/WebDLMLDAP/classes/WebDLMLDAP.php
@@ -87,7 +87,7 @@ class WebDLMLDAP extends WebDLMBase {
             $letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
             $rows = array();
             foreach ($letters as $letter) {
-                $filter = "(sAMAccountName=".$leter."*)";
+                $filter = "(sAMAccountName=".$letter."*)";
                 $tmp_rows = $this->search($filter, $col_ary, $col_to_id);
                 $rows = array_merge($rows, $tmp_rows);
             }
